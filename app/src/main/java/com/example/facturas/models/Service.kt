21d2305f323765a37bf714511d307124f605dd48
Óscar Gómez <oscargomezsedas@gmail.com>
2024-10-29 10:49:49 +0100

    changed service to use room

--- app/src/main/java/com/example/facturas/models/Service.kt
+++ app/src/main/java/com/example/facturas/models/Service.kt
@@ -1,14 +1,25 @@
 package com.example.facturas.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-data class Service (
-    val name: String,
+@Entity
+data class Service(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val description: String,
     val price: Double,
     val discount: Double,
     val units: Int,
     val tax: Double,
-    val subTotal: Double = BigDecimal(price * ((100 - discount) / 100)).setScale(2, RoundingMode.HALF_EVEN).toDouble(),
-    val total: Double = BigDecimal(subTotal * (((tax) / 100) + 1)).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+    val subTotal: Double = BigDecimal(price * ((100 - discount) / 100)).setScale(
+        2,
+        RoundingMode.HALF_EVEN
+    ).toDouble(),
+    val total: Double = BigDecimal(subTotal * (((tax) / 100) + 1)).setScale(
+        2,
+        RoundingMode.HALF_EVEN
+    ).toDouble()
 )
